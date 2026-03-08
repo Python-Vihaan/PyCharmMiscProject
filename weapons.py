2f7d12a94faf354cd5a3d881dbcc0a56a9b1f5c4
kumarroy <vihaanak11@gmail.com>
2026-03-08 18:29:35 -0500

    Added movement.

--- weapons.py
+++ weapons.py
@@ -1,4 +1,8 @@
 import random
+
+from Adventure import locations
+
+
 class Weapon:
     def __init__(self):
         self.power = 10
@@ -32,3 +36,33 @@ class Satchel:
 
 class Player:
     pass
+class Map:
+    def __init__(self,rows,columns):
+        self.rows = rows
+        self.columns = columns
+        self.row = [" "] * self.columns
+        self.the_map = [self.row.copy() for number in range(self.rows)]
+        self.the_map[0][self.columns//2] = "X"
+        self.player_location = [0,self.columns//2]
+    def move(self,direction):
+        self.player_location[self.player_location[0][self.player_location[1]] = " "
+        if direction == "u":
+            self.player_location[0]+= 1
+        elif direction == "d":
+            self.player_location[0]-= 1
+        elif direction == "l":
+            self.player_location[1]-= 1
+        elif direction == "r":
+            self.player_location[1] -= 1
+
+
+
+
+
+map = Map(20,20)
+while True:
+    direction = input("Which direction do you want to move? ")
+    map.move(direction)
+    for row in map.the_map:
+        print(row)
+
