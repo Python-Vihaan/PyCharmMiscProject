d0d9e5fcaffee1f877a1b428fa9deb44d20025eb
kumarroy <vihaanak11@gmail.com>
2026-03-29 18:39:38 -0500

    Having trouble with display.

--- weapons.py
+++ weapons.py
@@ -1,27 +1,38 @@
 import random
+import pygame
+from pygame.examples.grid import WINDOW_WIDTH, WINDOW_HEIGHT
 
-from Adventure import locations
+from fun import clock
+
+pygame.init()
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 10000
+screen = pygame.display.set_mode([WINDOW_WIDTH,WINDOW_HEIGHT])
+pygame.display.set_caption("The adventure")
+clock = pygame.time.Clock()
+FBS = 30
 
 
 class Weapon:
     def __init__(self):
         self.power = 10
+
     def use(self):
-        damage = random.randrange(0,21)
+        damage = random.randrange(0, 21)
         self.set_power()
         return damage
+
     def grant_special(self):
         pass
+
     def set_power(self):
         self.power -= 1
+
 
 sword = Weapon()
 damage = sword.use()
 print(damage)
 print(sword.power)
-
-
-
 
 
 class Monster:
@@ -31,37 +42,40 @@ class Monster:
 class Locations:
     pass
 
+
 class Satchel:
     pass
 
+
 class Player:
     pass
+
+
 class Map:
-    class Map:
-        def __init__(self, rows, columns):
-            self.rows = rows
-            self.columns = columns
-            self.row = [" "] * self.columns
-            self.the_map = [self.row.copy() for number in range(self.rows)]
-            self.the_map[0][self.columns // 2] = "X"
-            self.player_location = [0, self.columns // 2]
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
+        self.row = [" "] * self.columns
+        self.the_map = [self.row.copy() for number in range(self.rows)]
+        self.the_map[0][self.columns // 2] = "X"
+        self.player_location = [0, self.columns // 2]
 
-        def move(self, direction):
-            self.the_map[self.player_location[0]][self.player_location[1]] = " "
-            if direction == "u":
-                self.player_location[0] -= 1
-            elif direction == "d":
-                self.player_location[0] += 1
-            elif direction == "l":
-                self.player_location[1] -= 1
-            elif direction == "r":
-                self.player_location[1] += 1
-            self.the_map[self.player_location[0]][self.player_location[1]] = "X"
+    def move(self, direction):
+        self.the_map[self.player_location[0]][self.player_location[1]] = " "
+        if direction == "u":
+            self.player_location[0] -= 1
+        elif direction == "d":
+            self.player_location[0] += 1
+        elif direction == "l":
+            self.player_location[1] -= 1
+        elif direction == "r":
+            self.player_location[1] += 1
+        self.the_map[self.player_location[0]][self.player_location[1]] = "X"
 
-    map = Map(20, 20)
-    while True:
-        direction = input("Which direction do you want to move? ")
-        map.move(direction)
-        for row in map.the_map:
-            print(row)
 
+map = Map(20, 20)
+while True:
+    direction = input("Which direction do you want to move? ")
+    map.move(direction)
+    for row in map.the_map:
+        print(row)
